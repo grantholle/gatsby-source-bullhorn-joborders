5094cc6a5dc36c48300f4ad0a5067a687c9dc273
Grant Holle <hollegrant@gmail.com>
2019-10-18 21:48:54 +0800

    Add basic job fetching functionality and node creation

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,29 +1,28 @@
-const fetchJobOrders = require(`./fetch`)
+const { fetchJobs } = require(`./fetch`)
 
-exports.sourceNodes = (
+exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest },
   pluginOptions
 ) => {
   const { createNode } = actions
 
-  return Promise.map(pluginOptions.queries, ({ query, limit }) =>
-    fetchJobOrders({ query, limit }).then(results =>
-      results.forEach(result => {
-        result.jobOrderId = result.id
-        result.id = createNodeId(result.id)
+  const jobs = await fetchJobs(pluginOptions)
 
-        const node = {
-          ...result,
-          query,
-          parent: null,
-          children: [],
-          internal: {
-            type: `JobOrder`,
-          },
-        }
-        node.internal.contentDigest = createContentDigest(node)
-        createNode(node)
-      })
-    )
-  )
+  jobs.forEach(job => {
+    job.jobOrderId = job.id
+    job.id = createNodeId(job.id)
+
+    const node = {
+      ...job,
+      parent: null,
+      children: [],
+      internal: {
+        type: `JobOrder`,
+      },
+    }
+
+    node.internal.contentDigest = createContentDigest(node)
+
+    createNode(node)
+  })
 }
